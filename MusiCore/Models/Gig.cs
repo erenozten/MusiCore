79b4a5a5291f668f91e41ae6fc29e2fcec2ced2d
ErenOzten <priimalshade@gmail.com>
2020-03-25 00:18:31 +0300

    Created Gig Class and some necessary properties

---
+++ MusiCore/Models/Gig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusiCore.Models
+{
+    public class Gig
+    {
+        public int Id { get; set; }
+
+        // Konserin verileceği yer
+        public string Venue { get; set; }
+    }
+}
