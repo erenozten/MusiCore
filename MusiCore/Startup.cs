75dad8858cf3c6c31728517e560591c8fe6a6c9f
ErenOzten <priimalshade@gmail.com>
2020-03-24 16:39:31 +0300

    building mvc

--- MusiCore/Startup.cs
+++ MusiCore/Startup.cs
@@ -5,9 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MusiCore.Models;
+
 
 namespace MusiCore
 {
@@ -24,6 +27,10 @@ namespace MusiCore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<MusiCoreDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("MusiCoreConnectionString")));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
