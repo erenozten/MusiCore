ebc4180abff77d937c5d3dc112a069d872fdd76e
ErenOzten <priimalshade@gmail.com>
2020-03-31 15:23:28 +0300

    KitapAppDbContext ekle

---
+++ MusiCore/Models/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MusiCore.Models
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            //constructor'u bu şekilde yazmalıyız.
+        }
+
+        //public virtual DbSet<Book> Books { get; set; }
+        //public virtual DbSet<Genre> Genres { get; set; }
+        //public virtual DbSet<User> Users { get; set; }
+    }
+}
