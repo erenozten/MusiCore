f45f1af5feaa9157efb355fe4112b558a3a9b27e
ErenOzten <priimalshade@gmail.com>
2020-04-06 02:28:15 +0300

    appdbcontext inheritance (it works)

--- MusiCore/MusiCore/Data/ApplicationDbContext.cs
+++ MusiCore/MusiCore/Data/ApplicationDbContext.cs
@@ -3,19 +3,20 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using MusiCore.Models;
 
 namespace MusiCore.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
         //ben ekledim
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
-        }
+        //protected override void OnModelCreating(ModelBuilder builder)
+        //{
+        //    base.OnModelCreating(builder);
+        //}
     }
 }
