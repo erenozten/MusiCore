86a5ebe6c0566d1300d7cadacd3366008cf2c2bc
ErenOzten <priimalshade@gmail.com>
2020-04-09 23:23:47 +0300

    HomeController

--- MusiCore/MusiCore/Controllers/HomeController.cs
+++ MusiCore/MusiCore/Controllers/HomeController.cs
@@ -2,24 +2,39 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MusiCore.Models;
 
-namespace MusiCore.Controllers
+namespace MusiCore.Controllers 
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+
+        //    _logger = logger;
+        //}
+
+        private UserManager<ApplicationUser> _userManager;
+
+        //class constructor
+        public HomeController(UserManager<ApplicationUser> userManager)
         {
-            _logger = logger;
+            _userManager = userManager;
         }
+
+        private Task<ApplicationUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
 
         public IActionResult Index()
         {
+            System.Security.Claims.ClaimsPrincipal currentUser = this.User;
+            var id = _userManager.GetUserId(User); // Get user id:
             return View();
         }
 
