6c9e62aaf3aa34a74c312cd9da402ee42cf596e9
ErenOzten <priimalshade@gmail.com>
2020-04-15 01:07:03 +0300

    removed unused namespaces (using statements) from Genre class

--- MusiCore/MusiCore/Models/Genre.cs
+++ MusiCore/MusiCore/Models/Genre.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace MusiCore.Models
+﻿namespace MusiCore.Models
 {
     public class Genre
     {
