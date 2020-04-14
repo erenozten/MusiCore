b23ae6b2c933c669e85d9a9f4d61ecda4a55b2ff
ErenOzten <priimalshade@gmail.com>
2020-04-14 23:59:43 +0300

    Create TestController.cs

---
+++ MusiCore/MusiCore/Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MusiCore.Data;
+using MusiCore.Models;
+
+namespace MusiCore.Controllers
+{
+    public class TestController : Controller
+    {
+        private ApplicationDbContext _context;
+        //public TestController(ApplicationDbContext context)
+        //{
+        //    context = _context;
+        //}
+
+        public IActionResult Index()
+        {
+
+
+                ApplicationUser appUser = new ApplicationUser();
+                appUser.CustomTag = "asd";
+                _context.ApplicationUsers.Add(appUser);
+                _context.SaveChanges();
+            return View();
+        }
+    }
+}
