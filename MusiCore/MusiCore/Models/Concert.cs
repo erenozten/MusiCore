12da8538f2b3d76f47473b12fd8cf0ce515bc575
ErenOzten <priimalshade@gmail.com>
2020-04-15 01:04:44 +0300

    added new property to Concert class

--- MusiCore/MusiCore/Models/Concert.cs
+++ MusiCore/MusiCore/Models/Concert.cs
@@ -7,6 +7,7 @@ namespace MusiCore.Models
 {
     public class Concert
     {
+        public int Id { get; set; }
         public ApplicationUser Artist { get; set; }
         public DateTime DateTime { get; set; }
         public string Venue { get; set; }
