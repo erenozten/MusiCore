df47e7ef3a124407a0edef02ade2b9e4b0c11ea6
ErenOzten <priimalshade@gmail.com>
2020-04-17 17:20:15 +0300

    Added data-annotations for Genre class

--- MusiCore/MusiCore/Models/Genre.cs
+++ MusiCore/MusiCore/Models/Genre.cs
@@ -1,8 +1,13 @@
-﻿namespace MusiCore.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MusiCore.Models
 {
     public class Genre
     {
         public byte Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
     }
 }
