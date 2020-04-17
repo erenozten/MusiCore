6b1b938238c635e83112d96d2de50c5795a76c04
ErenOzten <priimalshade@gmail.com>
2020-04-17 17:21:12 +0300

    Comments added to Concert class

--- MusiCore/MusiCore/Models/Concert.cs
+++ MusiCore/MusiCore/Models/Concert.cs
@@ -7,14 +7,17 @@ namespace MusiCore.Models
     {
         public int Id { get; set; }
 
+        // Bir konser, kesinlikle bir artist'e sahip olmalıdır. Bu yüzden [Required] data-annotation'ı kullanıldı.
         [Required]
         public ApplicationUser Artist { get; set; }
         public DateTime DateTime { get; set; }
 
+        // Bir konser, kesinlikle bir venue'ye sahip olmalıdır. Bu yüzden [Required] data-annotation'ı kullanıldı.
         [Required]
         [StringLength(255)]
         public string Venue { get; set; }
 
+        // Bir konser, kesinlikle bir genre'ye sahip olmalıdır. Bu yüzden [Required] data-annotation'ı kullanıldı.
         [Required]
         public Genre Genre { get; set; }
     }
