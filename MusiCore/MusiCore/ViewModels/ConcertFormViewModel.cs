7b9af4615a88ee0a2ea371a02e5c183e3aed644e
ErenOzten <priimalshade@gmail.com>
2020-05-11 13:15:57 +0300

    created ConcertFormViewModel

--- MusiCore/MusiCore/ViewModels/ConcertFormViewModel.cs
+++ MusiCore/MusiCore/ViewModels/ConcertFormViewModel.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MusiCore.Models;
 
 namespace MusiCore.ViewModels
 {
     public class ConcertFormViewModel
     {
+        // property'leri string olarak oluşturduğumuza dikkat:
+
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; } // db'deki tüm Genre'leri dropdownlist'e eklemek için kullanacağımız property
     }
 }
