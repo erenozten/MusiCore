3eaee0a5dc1bee4e89912308aa0a2a71e9244f1e
ErenOzten <priimalshade@gmail.com>
2020-05-11 15:01:27 +0300

    added foreign keys to Concert class

--- MusiCore/MusiCore/Models/Concert.cs
+++ MusiCore/MusiCore/Models/Concert.cs
@@ -7,9 +7,12 @@ namespace MusiCore.Models
     {
         public int Id { get; set; }
 
-        // Bir konser, kesinlikle bir artist'e sahip olmalıdır. Bu yüzden [Required] data-annotation'ı kullanıldı.
-        [Required]
+        // Bir konser, kesinlikle bir artist'e sahip olmalıdır. Bu yüzden [Required] data-annotation'ı kullanıldı. Edit: Required attribute'unu Foreign Key olan ArtistId ye taşıdık. Buradaki required silindi.
         public ApplicationUser Artist { get; set; }
+
+        [Required]
+        public string ArtistId { get; set; }
+
         public DateTime DateTime { get; set; }
 
         // Bir konser, kesinlikle bir venue'ye sahip olmalıdır. Bu yüzden [Required] data-annotation'ı kullanıldı.
@@ -19,7 +22,9 @@ namespace MusiCore.Models
 
         // Bir konser, kesinlikle bir genre'ye sahip olmalıdır. Bu yüzden [Required] data-annotation'ı kullanıldı.
         // Foreign key'leri değil, navigasyon property'leri required yapmış olduk.
-        [Required]
         public Genre Genre { get; set; }
+
+       [Required]
+        public byte GenreId { get; set; }
     }
 }
