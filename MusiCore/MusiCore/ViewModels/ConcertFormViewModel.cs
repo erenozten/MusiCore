2e5fdad677ae41c1fb3425966d3d85be3fe99ad8
ErenOzten <priimalshade@gmail.com>
2020-05-12 13:08:07 +0300

    added some properties to ConcertFormViewModel

--- MusiCore/MusiCore/ViewModels/ConcertFormViewModel.cs
+++ MusiCore/MusiCore/ViewModels/ConcertFormViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using MusiCore.Models;
@@ -10,10 +11,27 @@ namespace MusiCore.ViewModels
     {
         // property'leri string olarak oluşturduğumuza dikkat:
 
+        [Required]
         public string Venue { get; set; }
+        
         public string Date { get; set; }
+        
         public string Time { get; set; }
-        public int Genre { get; set; }
+        
+        [Required]
+        public byte Genre { get; set; }
+        
+        //[Required]
         public IEnumerable<Genre> Genres { get; set; } // db'deki tüm Genre'leri dropdownlist'e eklemek için kullanacağımız property
+
+        //public DateTime DateTime
+        //{
+        //    get { return DateTime.Parse(string.Format("{0} {1}", Date, Time)); }
+        //}
+
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+        }
     }
 }
