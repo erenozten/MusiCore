d83a9cb64b2ec300a52a35b094fc3c1794e6587d
ErenOzten <priimalshade@gmail.com>
2020-05-12 15:12:02 +0300

    Update FutureDate.cs

--- MusiCore/MusiCore/ViewModels/FutureDate.cs
+++ MusiCore/MusiCore/ViewModels/FutureDate.cs
@@ -1,11 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace MusiCore.ViewModels
 {
-    public class FutureDate
+    public class FutureDate: ValidationAttribute
     {
+        public override bool IsValid(object arrivedDateOfDateTime)
+        {
+            DateTime newDateOfDateTime;
+
+            var isValid = DateTime.TryParseExact(Convert.ToString(arrivedDateOfDateTime),
+                "D MMM YYYY", 
+                CultureInfo.CurrentCulture, 
+                DateTimeStyles.None,
+                out newDateOfDateTime);
+
+            return (isValid && newDateOfDateTime > DateTime.Now);
+
+        }
     }
 }
