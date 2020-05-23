1c44dbd23eced366451983cdc626ed53b15bb778
ErenOzten <priimalshade@gmail.com>
2020-05-23 20:17:47 +0300

    Create TextsController.cs

---
+++ MusiCore/MusiCore/Controllers/TextsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MusiCore.Controllers
+{
+    public class TextsController : Controller
+    {
+        public IActionResult About()
+        {
+            // Proje ne işe yaramaktadır? Sorusunu cevaplayacak olan ActionResult
+            return View();
+        }
+
+        public IActionResult Technologies()
+        {
+            // Projede kullanılan teknolojilerin, tekniklerin belirtilecek olduğu ActionResult.
+            return View();
+        }
+
+        public IActionResult Logs()
+        {
+            // Projeye eklenen çıkarılan her şeyin log'landığı ActionResult.
+            return View();
+        }
+
+        public IActionResult Bosch()
+        {
+            return View();
+        }
+    }
+}
