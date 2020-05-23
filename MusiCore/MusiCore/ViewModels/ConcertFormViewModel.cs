a677c991a8a88893a5f222ea6c3c0968054771d6
ErenOzten <priimalshade@gmail.com>
2020-05-23 18:19:39 +0300

    Update ConcertFormViewModel.cs

--- MusiCore/MusiCore/ViewModels/ConcertFormViewModel.cs
+++ MusiCore/MusiCore/ViewModels/ConcertFormViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,20 +12,34 @@ namespace MusiCore.ViewModels
     {
         // property'leri string olarak oluşturduğumuza dikkat:
 
-        [Required]
+
+        //[MinLength(2, ErrorMessage = "Bu alan en az 2 karakterden oluşmalıdı.")]
+        //[StringLength(3000, ErrorMessage = "Bu alan en fazla 100 karakterden oluşmalıdı.")]
+        //[DisplayFormat(NullDisplayText = "Veri yok")]
+        //[Display(Name="Yer")]
+
+
+        [Required(ErrorMessage = "Bu alan boş bırakılamaz")]
+        [Display(Name = "Yer", Prompt = "Kadıköy")]
+        [MinLength(2, ErrorMessage = "Bu alan en az 2 karakterden oluşmalıdı.")]
+        [StringLength(3000, ErrorMessage = "Bu alan en fazla 100 karakterden oluşmalıdı.")]
         public string Venue { get; set; }
-        
-        [Required]
+
+        [Required(ErrorMessage = "Bu alan boş bırakılamaz")]
         [FutureDate]
+        [Display(Name = "Tarih", Prompt = "13 Jan 2023")]
+
         public string Date { get; set; }
-        
-        [Required]
+
+        [Required(ErrorMessage = "Bu alan boş bırakılamaz")]
         [FutureTime]
+        [Display(Name = "Saat", Prompt = "13 Jan 2023")]
         public string Time { get; set; }
-        
-        [Required]
+
+        [Required(ErrorMessage = "Bu alan boş bırakılamaz")]
+        [Display(Name = "Tür")]
         public byte Genre { get; set; }
-        
+
         public IEnumerable<Genre> Genres { get; set; } // db'deki tüm Genre'leri dropdownlist'e eklemek için kullanacağımız property
 
         public DateTime GetDateTime()
