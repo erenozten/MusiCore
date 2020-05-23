e7bc766bad9a7b1a0d1737457696fe37a9a141fd
ErenOzten <priimalshade@gmail.com>
2020-05-24 01:46:59 +0300

    Create UsedApproach.cs

---
+++ MusiCore/MusiCore/TextModels/UsedApproach.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusiCore.TextModels
+{
+    public class UsedApproach
+    {
+        public int Id { get; set; }
+        public string TextTurkish { get; set; }
+        public string TextEnglish { get; set; }
+    }
+}
