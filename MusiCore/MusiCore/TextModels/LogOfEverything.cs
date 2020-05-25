0392bc3f3aa323166bbf2225a4982e0b260542ca
ErenOzten <priimalshade@gmail.com>
2020-05-25 14:11:10 +0300

    Update LogOfEverything.cs

--- MusiCore/MusiCore/TextModels/LogOfEverything.cs
+++ MusiCore/MusiCore/TextModels/LogOfEverything.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,11 @@ namespace MusiCore.TextModels
     public class LogOfEverything
     {
         public int Id { get; set; }
+
+        [Display(Name = "Türkçe")]
         public string TextTurkish { get; set; }
+
+        [Display(Name = "İngilizce")]
         public string TextEnglish { get; set; }
     }
 }
