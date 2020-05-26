626f705847f4bb192d19403eb384d90496e646db
ErenOzten <priimalshade@gmail.com>
2020-05-26 12:20:57 +0300

    tasks to do as comments

--- MusiCore/MusiCore/Controllers/TextController.cs
+++ MusiCore/MusiCore/Controllers/TextController.cs
@@ -8,7 +8,7 @@ namespace MusiCore.Controllers
 {
     public class TextController : Controller
     {
-        public IActionResult About()
+        public IActionResult AboutProject()
         {
             // Proje ne işe yaramaktadır? Sorusunu cevaplayacak olan ActionResult
             return View();
@@ -18,16 +18,15 @@ namespace MusiCore.Controllers
         {
             // Projede kullanılan teknolojilerin, tekniklerin belirtilecek olduğu ActionResult.
 
-            // Proje .NET CORE 3.1 ile oluşturuldu.
-            // Entity Framework kullanıldı.
-            // Bootstrap kullanıldı.
-            // Code-First Yaklaşımı kullanıldı.
-            // Migration'lar kullanıldı.
-            // Data Validation (Client Side Validation)
-            // ASP.NET Identity (Sign Up, Login, Log out, Change Password etc.) 
-
-            // Security Issues
-            // Artistic UI
+            // Proje .NET CORE 3.1 ile oluşturuldu //db'ye eklendi
+            // Entity Framework kullanıldı //db'ye eklendi
+            // Code-First Yaklaşımı kullanıldı //db'ye eklendi
+            // Migration'lar kullanıldı //db'ye eklendi
+            // Client Side Validation entegrasyonu yapıldı //db'ye eklendi
+            // ASP.NET Identity entegrasyonu yapıldı (Kaydol, Giriş Yap, Çıkış Yap, Şifre Değiştir) //db'ye eklendi
+            // Partial View'lar kullanıldı //db'ye eklendi
+            // Bootstrap kullanıldı //db'ye eklendi
+                
             return View();
         }
 
@@ -35,8 +34,9 @@ namespace MusiCore.Controllers
         {
             // Projede benimsenen yaklaşımlarla ilgili kısa notların yazıldığı ActionResult.
 
-            // Migration'lar küçük parçalar halinde oluşturuldu. Dolayısıyla veritabanı problemlerinin yaşanma olasılığı azaltıldı.
-            // Proje bir müzik projesi olduğu için, yani gençlere hitap edeceği için tasarımda canlı renklere yer verildi.
+            // Migration'lar küçük parçalar halinde oluşturuldu. Dolayısıyla veritabanı problemleri yaşama olasılığı azaltıldı //db'ye eklendi
+            // Proje bir müzik projesi olduğu için, yani gençlere hitap edeceği için tasarımda canlı renklere yer verildi //db'ye eklendi
+            // Artistik tasarım uygulanmaya çalışıldı //db'ye eklendi
 
             return View();
         }
@@ -70,17 +70,20 @@ namespace MusiCore.Controllers
             // Create [HttpPost] ActionResult'ı oluşturuldu ve ilk Concert kaydı veritabanına eklenmiş oldu. Good job bruh.
             // Create sayfası için client side validation oluşturuldu. Bunun için jQueryVal kullanıldı.
             // Loglama, kullanılan teknolojiler vb için birer tablo oluşturuldu. Bu bilgiler veritabanında tutulacak.
+            // Partial view'lar ekleniyor.
+            // Tasarım düzenleniyor. Bol bol CSS, HTML Chore...
             return View();
         }
 
         public IActionResult CampaignOfAddingNewModule()
         {
-            // TextController'da bulunan tüm actionresultlar için bir tablo oluşturalım, görev yapıldıysa true yapalım property'sini, değilse false.
-            // Yapılmış olan görevler yeşil, yapılmamışlar kırmızı renk ile gösterilsin.
-            // Buton ile bu görevleri "yapıldı" diye işaretleyebilelim. Ajax post yapılabilir bu noktada.
+            // TextController'da bulunan tüm actionresultlar için bir tablo oluşturalım, görev yapıldıysa true yapalım property'sini, değilse false. //db'ye eklendi
+            // Yapılmış olan görevler yeşil, yapılmamışlar kırmızı renk ile gösterilsin //db'ye eklendi
+            // Buton ile bu görevleri "yapıldı" diye işaretleyebilelim. //db'ye eklendi
+            // Görevleri "yapıldı, yapılmadı" olarak işaretleyebilme olayı Ajax post yapılabilir.
             // Görevlerin Türkçe ve İngilizce diye property'leri olsun. İngilizceye çevirdiklerimizi ingilizce property'sinde tutacağız.
             // Change Password
-            // Add a gig
+            // Sisteme konserler eklensin // db'ye eklendi
             // My Upcoming Gigs
             // Edit a gig
             // Remove a gig
@@ -97,11 +100,14 @@ namespace MusiCore.Controllers
             // Gig Feed
             // CSS style ları
             // Genre için CRUD işlemleri yapılmak üzere admin paneli oluşturulmalı
+            // Security Issues (XSS falan, bunları ekleyelim)
+
             return View();
         }
 
         public IActionResult CampaignOfFixation()
         {
+            // Index sayfalarında tablo çok genişleyebiliyor property'lerin uzunluğuna göre. Bunlar düzenlenmeli. Trim metotları kullanılmalı.
             return View();
         }
     }
