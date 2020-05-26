afe438a698d7dcd54c3d75871caf96e0e2593a0a
ErenOzten <priimalshade@gmail.com>
2020-05-27 00:21:12 +0300

    added migrations for DateCreatedOrCompleted and CustomTag

---
+++ MusiCore/MusiCore/Data/Migrations/20200526111211_Neww.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MusiCore.Data.Migrations
+{
+    public partial class Neww : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateCompleted",
+                table: "CampaignOfAddingNewModules",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateCompleted",
+                table: "CampaignOfAddingNewModules");
+        }
+    }
+}
