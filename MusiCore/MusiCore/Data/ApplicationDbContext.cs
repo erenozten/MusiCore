dbd77d1ed85954fdc967aef57c7e2383f26ff6f0
ErenOzten <priimalshade@gmail.com>
2020-05-26 12:21:19 +0300

    some bug in DbContext.. Fixed.

--- MusiCore/MusiCore/Data/ApplicationDbContext.cs
+++ MusiCore/MusiCore/Data/ApplicationDbContext.cs
@@ -4,6 +4,7 @@ using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using MusiCore.Models;
+using MusiCore.TextModels;
 
 namespace MusiCore.Data
 {
@@ -17,6 +18,12 @@ namespace MusiCore.Data
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         public DbSet<Concert> Concerts { get; set; }
         public DbSet<Genre> Genres { get; set; }
+
+        public DbSet<CampaignOfAddingNewModule> CampaignOfAddingNewModules { get; set; }
+        public DbSet<LogOfEverything> LogOfEverythings { get; set; }
+        public DbSet<UsedApproach> UsedApproaches { get; set; }
+        public DbSet<UsedTechnology> UsedTechnologies { get; set; }
+        public DbSet<CampaignOfFixation> CampaignOfFixations { get; set; }
 
         //ben ekledim
         //protected override void OnModelCreating(ModelBuilder builder)
