7f02a65d825a295e87f54eb2fa3d2d932fcaad6e
ErenOzten <priimalshade@gmail.com>
2020-05-28 00:23:00 +0300

    Added DateCreated Property to LogOfEverything

--- MusiCore/MusiCore/TextModels/LogOfEverything.cs
+++ MusiCore/MusiCore/TextModels/LogOfEverything.cs
@@ -8,6 +8,10 @@ namespace MusiCore.TextModels
 {
     public class LogOfEverything
     {
+        public LogOfEverything()
+        {
+            DateCreated = DateTime.Now;
+        }
         public int Id { get; set; }
 
         [Display(Name = "Türkçe")]
@@ -15,5 +19,17 @@ namespace MusiCore.TextModels
 
         [Display(Name = "İngilizce")]
         public string TextEnglish { get; set; }
+
+        // private set'i kaldırabilirsin diyo intellisense. Lakin EntityFramework kolonu düşürüyor, eğer setter'i silersen.
+        // private set yaptık, çünkü dışarıdan şunun yapılmasını istemiyoruz: usedTechnology.DateCreatedAndCompleted = bla bla...  -> Bu yapılamamalı. Çünkü bu mantıksız bi işlem. Bu property zaten verinin oluştuğu anki tarihi tutuyor. Neden başka bir değer, bu değere set edilebilsin. O yüzden private set yaptık. Private yaptığımızdan ötürü ctor içinde Datetime.Now'a eşitleyebildik. Easy.
+        public DateTime DateCreated { get; private set; }
+
+        public string GetMonth
+        {
+            get
+            {
+                return "(" + ((DateTime.Now.Month + DateTime.Now.Year * 12) - (DateCreated.Month + DateCreated.Year * 12)).ToString() + " ay önce) ";
+            }
+        }
     }
 }
