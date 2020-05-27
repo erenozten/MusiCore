553ae4c37955788327869bf665b8ffba939685a6
ErenOzten <priimalshade@gmail.com>
2020-05-28 00:18:25 +0300

    updated usedTechnology.cs (added DateCreatedAndCompleted prop)

--- MusiCore/MusiCore/TextModels/UsedTechnology.cs
+++ MusiCore/MusiCore/TextModels/UsedTechnology.cs
@@ -8,10 +8,10 @@ namespace MusiCore.TextModels
 {
     public class UsedTechnology
     {
-        public UsedTechnology()
-        {
-            DateCreatedAndCompleted = DateTime.Now;
-        }
+        //public UsedTechnology()
+        //{
+        //    DateCreatedAndCompleted = DateTime.Now;
+        //}
         public int Id { get; set; }
 
         [Display(Name = "Türkçe")]
@@ -20,8 +20,16 @@ namespace MusiCore.TextModels
         [Display(Name = "İngilizce")]
         public string TextEnglish { get; set; }
 
-        // private set'i kaldırabilirsin diyo intellisense. Lakin EntityFramework kolonu düşürüyor, eğer setter'i silersen.
-        // private set yaptık, çünkü dışarıdan şunun yapılmasını istemiyoruz: usedTechnology.DateCreatedAndCompleted = bla bla...  -> Bu yapılamamalı. Çünkü bu mantıksız bi işlem. Bu property zaten verinin oluştuğu anki tarihi tutuyor. Neden başka bir değer, bu değere set edilebilsin. O yüzden private set yaptık. Private yaptığımızdan ötürü ctor içinde Datetime.Now'a eşitleyebildik. Easy.
-        public DateTime DateCreatedAndCompleted { get; private set; } 
+        //// private set'i kaldırabilirsin diyo intellisense. Lakin EntityFramework kolonu düşürüyor, eğer setter'i silersen.
+        //// private set yaptık, çünkü dışarıdan şunun yapılmasını istemiyoruz: usedTechnology.DateCreatedAndCompleted = bla bla...  -> Bu yapılamamalı. Çünkü bu mantıksız bi işlem. Bu property zaten verinin oluştuğu anki tarihi tutuyor. Neden başka bir değer, bu değere set edilebilsin. O yüzden private set yaptık. Private yaptığımızdan ötürü ctor içinde Datetime.Now'a eşitleyebildik. Easy.
+        //public DateTime DateCreatedAndCompleted { get; private set; }
+
+        //public string GetMonth
+        //{
+        //    get
+        //    {
+        //        return "(" + ((DateTime.Now.Month + DateTime.Now.Year * 12)  - (DateCreatedAndCompleted.Month + DateCreatedAndCompleted.Year * 12)).ToString() + " ay önce) ";
+        //    }
+        //}
     }
 }
