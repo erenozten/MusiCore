290e35470ab87a88519839d82928916cca0b8fb8
ErenOzten <priimalshade@gmail.com>
2020-05-28 14:19:29 +0300

    added one-to-many relationship with Fluent API

--- MusiCore/MusiCore/Data/ApplicationDbContext.cs
+++ MusiCore/MusiCore/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -26,9 +27,28 @@ namespace MusiCore.Data
         public DbSet<CampaignOfFixation> CampaignOfFixations { get; set; }
 
         //ben ekledim
-        //protected override void OnModelCreating(ModelBuilder builder)
-        //{
-        //    base.OnModelCreating(builder);
-        //}
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            // google'dan: If you want to change the delete behavior for all relationships, then you can use this code in OnModelCreating(...) to bulk set it for all relationships in your model.
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+
+            modelBuilder.Entity<Attendance>()
+                .HasOne(e => e.Concert)
+                .WithMany(c => c.Attendances);
+
+            modelBuilder.Entity<Attendance>()
+                .HasKey(c => new { c.ConcertId, c.AttendeeId });
+
+            //modelBuilder.Entity<Concert>()
+            //    .HasOne(p => p.Relationship)
+            //    .IsRequired(); 
+
+            //ModelBuilder.Entity<Attendance>().has
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
