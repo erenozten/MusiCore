4be80da16689f330a444dd19d404a45fff700b3c
ErenOzten <priimalshade@gmail.com>
2020-05-28 14:17:40 +0300

    created Attendance Class

--- MusiCore/MusiCore/Models/Attendance.cs
+++ MusiCore/MusiCore/Models/Attendance.cs
@@ -10,16 +10,17 @@ namespace MusiCore.Models
 {
     public class Attendance
     {
-        public Concert Concert { get; set; }
-
         [Key]
         [Column(Order = 1)]
         public int ConcertId { get; set; }
 
-        public ApplicationUser Attendee { get; set; }
-
         [Key]
         [Column(Order = 2)]
         public string AttendeeId { get; set; }
+
+        public Concert Concert { get; set; }
+
+        public ApplicationUser Attendee { get; set; }
+
     }
 }
