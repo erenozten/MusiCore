b329a546975e8557f25479afadf206f786057634
ErenOzten <priimalshade@gmail.com>
2020-05-30 19:17:42 +0300

    update Attendace API controller

--- MusiCore/MusiCore/Controllers/AttendanceController.cs
+++ MusiCore/MusiCore/Controllers/AttendanceController.cs
@@ -1,76 +1,73 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Security.Claims;
-//using System.Threading.Tasks;
-//using Microsoft.AspNetCore.Authorization;
-//using Microsoft.AspNetCore.Http;
-//using Microsoft.AspNetCore.Identity;
-//using Microsoft.AspNetCore.Mvc;
-//using MusiCore.Data;
-//using MusiCore.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MusiCore.Data;
+using MusiCore.Models;
 
-//namespace MusiCore.Controllers
-//{
-//    [Route("api/[controller]")]
-//    [ApiController]
-//    [Authorize]
-//    public class AttendanceController : ControllerBase
-//    {
-//        private ApplicationDbContext _context;
-//        //private readonly UserManager<ApplicationUser> _userManager;
+namespace MusiCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class AttendanceController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+        //private readonly UserManager<ApplicationUser> _userManager;
 
-//        public AttendanceController(ApplicationDbContext context)
-//        {
-//            _context = context;
-//        }
+        public AttendanceController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
 
-//        //private ApplicationDbContext _context;
-//        //private readonly UserManager<ApplicationUser> _userManager;
+        //private ApplicationDbContext _context;
+        //private readonly UserManager<ApplicationUser> _userManager;
 
-//        //public AttendanceController(UserManager<ApplicationUser> userManager, ApplicationDbContext context) //farklı iki constructor problem oluşturuyor. Tek contstructor ile userManager ve context alındı.
-//        //{
-//        //    _context = context;
-//        //    _userManager = userManager;
-//        //}
+        //public AttendanceController(UserManager<ApplicationUser> userManager, ApplicationDbContext context) //farklı iki constructor problem oluşturuyor. Tek contstructor ile userManager ve context alındı.
+        //{
+        //    _context = context;
+        //    _userManager = userManager;
+        //}
 
-//        //POST: api/TodoItems
-//       [HttpPost]
-//        public async Task<ActionResult> Attend(int concertId)
-//        {
-//            // currentUser, concertId'ye sahip olan konsere katılıyor.
-//            // getUserId ile currentUser'in Id'si alınır.
-//            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier); // will give the user's userId
+        [HttpPost]
+        public async Task<ActionResult> Attend(int concertId)
+        {
+            // currentUser, concertId'ye sahip olan konsere katılıyor.
+            // getUserId ile currentUser'in Id'si alınır.
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier); // will give the user's userId
 
-//            // var attendance = new attendance
-//            Attendance attendance = new Attendance();
+            // var attendance = new attendance
+            Attendance attendance = new Attendance();
 
-//            // attendance.userId = currentUserId
-//            // attendance.ConcertId = concertId
-//            attendance.ConcertId = concertId;
-//            attendance.AttendeeId = userId;
+            // attendance.userId = currentUserId
+            // attendance.ConcertId = concertId
+            attendance.ConcertId = concertId;
+            attendance.AttendeeId = userId;
 
-//            // _context.attendances.add(attendance) and SaveChanges.
-//            // Done.
+            _context.Attendances.Add(attendance);
+            await _context.SaveChangesAsync();
 
-//            //_context.TodoItems.Add(todoItem);
-//            //await _context.SaveChangesAsync();
+            ////return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
+            //return CreatedAtAction(concertId);
+            return Ok();
+        }
 
-//            ////return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
-//            return CreatedAtAction();
-//        }
+        // POST: api/TodoItems
+        //[HttpPost]
+        //public async Task<ActionResult> PostTodoItem(TodoItem todoItem)
+        //{
+        //    _context.TodoItems.Add(todoItem);
+        //    await _context.SaveChangesAsync();
 
-//        // POST: api/TodoItems
-//        //[HttpPost]
-//        //public async Task<ActionResult> PostTodoItem(TodoItem todoItem)
-//        //{
-//        //    _context.TodoItems.Add(todoItem);
-//        //    await _context.SaveChangesAsync();
-
-//        //    //return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
-//        //    return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.Id }, todoItem);
-//        //}
-//    }
+        //    //return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
+        //    return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.Id }, todoItem);
+        //}
+    }
 
 
-//}
+}
