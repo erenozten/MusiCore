7e8666e1c926151334eaab487f207e810e186ec8
ErenOzten <priimalshade@gmail.com>
2020-06-02 14:54:34 +0300

    created a view model named ConcertsViewModel for Index page

---
+++ MusiCore/MusiCore/ViewModels/ConcertsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MusiCore.Models;
+
+namespace MusiCore.ViewModels
+{
+    public class ConcertsViewModel
+    {
+        public IEnumerable<Concert> UpcomingConcerts { get; set; }
+        public bool ShowActions { get; set; }
+    }
+}
