7a4ea4febfbfbcde70625d483fc3bd61f8891365
Eren ÖZTEN <priimalshade@gmail.com>
2020-11-08 19:56:30 +0300

    Updates Init

---
+++ GigHub/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using GigHub.Core;
+using GigHub.Core.ViewModels;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GigHub.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        // ctor
+        public HomeController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        // Index action'ı çalıştığında query null olmalı.
+        public ActionResult Index(string query = null) 
+        {
+            var upcomingGigs = _unitOfWork.Gigs.GetUpcomingGigs(query);
+
+            // kullanıcı id'sini aldık.
+            var userId = User.Identity.GetUserId();
+
+
+            var attendances = _unitOfWork.Attendances.GetFutureAttendances(userId)
+                .ToLookup(a => a.GigId);
+
+            // Viewmodel oluşturup veriler içine yerleştirildi.
+            var viewModel = new GigsViewModel
+            {
+                UpcomingGigs = upcomingGigs,
+                ShowActions = User.Identity.IsAuthenticated,
+                Heading = "Upcoming Gigs",
+                SearchTerm = query,
+                Attendances = attendances
+            };
+
+            return View("Gigs", viewModel);
+        }
+
+        // Hakkında
+        public ActionResult About()
+        {
+            ViewBag.Message = "Hakkında";
+
+            return View();
+        }
+
+        // İletişim
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "İletişim";
+
+            return View();
+        }
+    }
+}
---
+++ GigHub/Core/Models/Gig.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace GigHub.Core.Models
+{
+    // Gig: Konser
+    public class Gig
+    {
+        public int Id { get; set; }
+
+        public bool IsCanceled { get; private set; }
+
+        // Sisteme giriş yapan kullanıcı
+        public ApplicationUser Artist { get; set; }
+
+        // Sisteme giriş yapan kullanıcının id değeri (string tipini kullanıyoruz)
+        public string ArtistId { get; set; }
+
+        // Konserin verilme zamanı
+        public DateTime DateTime { get; set; }
+
+        // Konserin verileceği yer
+        public string Venue { get; set; }
+
+        // Konserin türü
+        public Genre Genre { get; set; }
+
+        // Konserin türünün id değeri
+        public byte GenreId { get; set; }
+
+        // Konsere yapılan katılımlar. Dışarıdan bir liste set edilmesin diye private set olarak ayarlandı. Dolayısıyla eski liste, yeni bir listeyle değiştirilemeyecektir.
+        public ICollection<Attendance> Attendances { get; private set; }
+
+        // ctor. Konser oluşturulduğu anda yeni bir Attendances initialize etmemiz gerekiyor. Bunu yaptık.
+        public Gig()
+        {
+            Attendances = new Collection<Attendance>();
+        }
+
+        // Konser iptal edilirse Iscanceled = true olsun. Çünkü iptal edilen konseri sistemden silmeyeceğiz.
+        public void Cancel()
+        {
+            IsCanceled = true;
+
+            // Konserin iptal edildiğine dair bir bildirim oluşturuyoruz.
+            var notification = Notification.GigCanceled(this);
+
+            // Yukarıda oluşturulmuş olan "konser iptal edildi" içeriğine sahip bildirimi, konsere katılacak olan kullanıcılara gönderiyoruz.
+            foreach (var attendee in Attendances.Select(a => a.Attendee))
+            {
+                attendee.Notify(notification);
+            }
+        }
+
+        public void Modify(DateTime dateTime, string venue, byte genre)
+        {
+            var notification = Notification.GigUpdated(this, DateTime, Venue);
+
+            Venue = venue;
+            DateTime = dateTime;
+            GenreId = genre;
+
+            foreach (var attendee in Attendances.Select(a => a.Attendee))
+                attendee.Notify(notification);
+        }
+    }
+}
